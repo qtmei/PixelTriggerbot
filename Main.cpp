75a841dfba1557259b652334de427f63523a4df4
May <92554133+qtmei@users.noreply.github.com>
2025-07-23 01:19:52 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,36 +1,38 @@
 #include <Windows.h>
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
+const int PIXEL_COUNT = 4, THRESHOLD = 16;
 RECT screen;
 
 void GetPixels(COLORREF* pixels)
 {
 	int index = 0;
 
-	for (int x = 0; x < 2; x++)
-		for (int y = 0; y < 2; y++)
+	for (int x = -1; x < 2; x += 2)
+		for (int y = -1; y < 2; y += 2)
 			pixels[index++] = GetPixel(GetDC(GetActiveWindow()), (int)screen.right / 2 + x, (int)screen.bottom / 2 + y);
 }
 
-COLORREF GetAveragePixelColor(COLORREF* pixels)
+COLORREF GetAveragePixelColor(COLORREF* pixels) //calculate average RGB values of 4 pixels
 {
 	int tempR = 0, tempG = 0, tempB = 0;
 
-	for(int i = 0; i < 4; i++)
+	for(int i = 0; i < PIXEL_COUNT; i++)
 	{
 		tempR += (int)GetRValue(pixels[i]);
 		tempG += (int)GetGValue(pixels[i]);
 		tempB += (int)GetBValue(pixels[i]);
 	}
 
-	return RGB(tempR / 4, tempG / 4, tempB / 4);
+	return RGB(tempR / PIXEL_COUNT, tempG / PIXEL_COUNT, tempB / PIXEL_COUNT);
 }
 
-bool IsOutOfThreshold(COLORREF old, COLORREF current, int threshold)
+bool IsOutOfThreshold(COLORREF old, COLORREF current)
 {
-	return (int)GetRValue(old) > (int)GetRValue(current) + threshold || (int)GetRValue(old) < (int)GetRValue(current) - threshold || (int)GetGValue(old) > (int)GetGValue(current) + threshold || (int)GetGValue(old) < (int)GetGValue(current) - threshold || (int)GetBValue(old) > (int)GetBValue(current) + threshold || (int)GetBValue(old) < (int)GetBValue(current) - threshold;
+	return (int)GetRValue(old) > (int)GetRValue(current) + THRESHOLD || (int)GetRValue(old) < (int)GetRValue(current) - THRESHOLD || (int)GetGValue(old) > (int)GetGValue(current) + THRESHOLD || (int)GetGValue(old) < (int)GetGValue(current) - THRESHOLD || (int)GetBValue(old) > (int)GetBValue(current) + THRESHOLD || (int)GetBValue(old) < (int)GetBValue(current) - THRESHOLD;
 }
 
 void Attack()
@@ -44,7 +46,7 @@ void Attack()
 
 int main()
 {
-	COLORREF *oldPixels = new COLORREF[4], *currentPixels = new COLORREF[4];
+	COLORREF *oldPixels = new COLORREF[PIXEL_COUNT], *currentPixels = new COLORREF[PIXEL_COUNT];
 
 	cout << "© 2021 Meiware.net\nvisually examines pixels and simulates key press (no memory modification)\nhold 'Q' for triggerbot" << endl;
 
@@ -58,14 +60,14 @@ int main()
 		{
 			GetPixels(currentPixels); //get current pixels
 
-			if (IsOutOfThreshold(GetAveragePixelColor(currentPixels), GetAveragePixelColor(oldPixels), 16)) //compare old and current pixels
+			if (IsOutOfThreshold(GetAveragePixelColor(currentPixels), GetAveragePixelColor(oldPixels))) //compare old and current pixels
 				Attack();
 		}
 
-		Sleep(4);
+		Sleep(5);
 	}
 
-	delete[] currentPixels;
+	delete[] currentPixels; //garbage collection
 	delete[] oldPixels;
 
 	return 0;
